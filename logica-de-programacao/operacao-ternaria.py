a87f7fc71934b2b8cf498febe3b403ce5179a46c
iprih <priscila.silva076@gmail.com>
2020-01-11 16:01:03 -0300

    aula 35

---
+++ logica-de-programacao/operacao-ternaria.py
@@ -0,0 +1,24 @@
+"""
+logged_user = False
+
+if logged_user == True:
+    msg = 'Usuario logado'
+else:
+    msg = 'Usuario precisa logar'
+
+print(msg)
+"""
+# usando o mesmo conceito com ternario
+logged_user = False
+msg = 'Usuario logado.' if logged_user else 'Usuario precisa logar.'
+print(msg)
+
+# outro exemplo
+idade = input('Qual sua idade? ')
+if not idade.isnumeric():
+    print('Voce precisa digitar apenas números')
+else:
+    idade = int(idade)
+    maior_de_idade = (idade >= 18)
+    msg2 = 'Pode acessar' if maior_de_idade else 'Nao pode acessar.'
+    print(msg2)
